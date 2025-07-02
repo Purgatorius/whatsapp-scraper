3b7aa977473803200b04c15c9be7c5d965e6ecb5
Purgatorius <Bartlomiej.Bugala@gmail.com>
2025-07-02 15:50:37 +0200

    created WhatsAppScraper class in scraper.py ; added few initial method stubs inside WhatsAppScraper class

---
+++ evidence_scraper/scraper.py
@@ -0,0 +1,10 @@
+from evidence_scraper.locators import Locators
+
+class WhatsAppScraper:
+    def __init__()
+    def run()
+    def go_to_WhatsAppWeb()
+    def accept_cookie_popupi()
+    def select_chat()
+    def extract_messages()
+    
