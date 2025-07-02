42dbb98bb444c593c8c815810da88f15503d2302
Purgatorius <Bartlomiej.Bugala@gmail.com>
2025-07-02 15:16:50 +0200

    Added ENUM classes in locators.py - GoogleLocators() and WhatsAppLocators()

---
+++ evidence_scraper/locators.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class GoogleLocators(Enum):
+    ACCEPT_POLISH = "button:has-text('Zaakceptuj wszystko')"
+    ACCEPT_ENGLISH = "button:has-text('Accept all')"
+    SEARCH_BOX = '[name="q"]'
+
+class WhatsAppLocators(Enum):
+    HOME_URL = "https://www.whatsapp.com/?lang=pl_PL"
+    WEB_LINK = "text=WhatsApp Web"
+    CONTINUE_BUTTON = "text=Continue"
+    CHAT_LIST_ITEM = '[aria-label="Chat list"] [role="listitem"]'
+    MESSAGE_SPAN = 'span[dir="ltr"]'
+    OK_BUTTON = 'button:has-text("OK")'
+    CLOSE_BUTTON = '[aria-label="Close"]'
+    CHAT_ARROW_DOWN = '[data-icon="ic-chevron-down-wide"]'
+
