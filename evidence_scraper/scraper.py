357d86e55ecd44e4c4990907653b9bded5439b72
Purgatorius <Bartlomiej.Bugala@gmail.com>
2025-07-03 15:07:47 +0200

    feat: added WhatsAppScraper() class constructor

--- evidence_scraper/scraper.py
+++ evidence_scraper/scraper.py
@@ -1,10 +1,19 @@
 from evidence_scraper.locators import Locators
+from evidence_scraper.playwright_runner import PlaywrightRunner
 
 class WhatsAppScraper:
-    def __init__()
-    def run()
+    def __init__(self, headless=True):
+        self.runner = PlaywrightRunner(headless=headless)
+        self.page = self.runner.page
+
+    def run(self):
+        self.go_to_WhatsAppWeb()
+        self.accept_coockie_popup()
+        self.select_chat()
+        self.extract_messages()
+        self.runner.close()
+
     def go_to_WhatsAppWeb()
     def accept_cookie_popupi()
     def select_chat()
     def extract_messages()
-    
